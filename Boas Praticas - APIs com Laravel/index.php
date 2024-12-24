1f8752013a075ee0ab9bc05e8350999e48cfed1d
Gio-Midonz-Bots <roboticagiovanna@gmail.com>
2024-12-23 21:30:02 -0300

    Primeira Aula

---
+++ Boas Praticas - APIs com Laravel/index.php
@@ -0,0 +1,17 @@
+<?php  
+    //echo "Hello Dio";//print na tela
+    // $Cursos =30; //variável
+
+    $valorDia = 80;
+    $gorjetas = 40;
+
+    $diaria = $valorDia + $gorjetas;
+
+    echo "Sengunda-feira :  $diaria\n";
+    $gorjetas = 15;
+
+    $diaria = $valorDia + $gorjetas;
+
+    echo "Terça-feira :  $diaria";
+
+?>
